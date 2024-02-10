c36456612b4130cbb4367f1dd8e7256eddf2368a
Chebs Awah <chebesiawah@gmail.com>
2024-02-10 11:29:37 +0100

    roadmap section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,86 +1,86 @@
 import React from 'react'
 import './home.css'
 import { Link } from 'react-router-dom'
-import { diagram, roadmap, testnet,navbarLogo, token } from '../../assets'
+import { diagram, roadmap, testnet, navbarLogo, token } from '../../assets'
 
 const Home = () => {
   return (
     <div className="home">
       <div className="home__top-vectors">
-      <div className="home__bottom-ball"></div>
-      <div className="home__top-flash"></div>
-      <div className="home__top-stars"></div>
+        <div className="home__bottom-ball"></div>
+        <div className="home__top-flash"></div>
+        <div className="home__top-stars"></div>
       </div>
       <div className="home__hero">
-      <div className="home__hero-left">
-      <div className='navbar'>
-      <div className="navbar__navbar-logo">
-        <img src={navbarLogo} alt="" />
-      </div>
-    </div>
-        <div className="home__hero-header">
-          <span className="home__hero-headerText">
-          Welcome to Nova
-          </span>
-          <div className="hero-text-border"></div>
+        <div className="home__hero-left">
+          <div className='navbar'>
+            <div className="navbar__navbar-logo">
+              <img src={navbarLogo} alt="" />
+            </div>
+          </div>
+          <div className="home__hero-header">
+            <span className="home__hero-headerText">
+              Welcome to Nova
+            </span>
+            <div className="hero-text-border"></div>
+          </div>
+          <h1 className="home__hero-message">
+            Unlock the <span className="gradient">Future</span> of <br />
+            <span className="gradient">Financial Staking with </span> <br />
+            Nova
+          </h1>
+          <Link to="/" className='home__hero-link'>Stake Now</Link>
+          <div className="hero__bottom-vectors">
+            <div className="hero__full-ball"></div>
+            <div className="hero__bottom-flash"></div>
+            <div className="hero__bottom-second-flash"></div>
+            <div className="hero__bottom-big-star"></div>
+            <div className="hero__bottom-small-star"></div>
+            <div className="hero__bottom-planet"></div>
+          </div>
         </div>
-        <h1 className="home__hero-message">
-        Unlock the <span className="gradient">Future</span> of <br />
-<span className="gradient">Financial Staking with </span> <br />   
-Nova
-        </h1>
-        <Link to="/" className='home__hero-link'>Stake Now</Link>
-        <div className="hero__bottom-vectors">
-      <div className="hero__full-ball"></div>
-      <div className="hero__bottom-flash"></div>
-      <div className="hero__bottom-second-flash"></div>
-      <div className="hero__bottom-big-star"></div>
-      <div className="hero__bottom-small-star"></div>
-      <div className="hero__bottom-planet"></div>
-      </div>
-      </div>
-      <div className="home__hero-right">
-              <div className="home__hero-right-image">
-                
-              </div>
-      </div>
+        <div className="home__hero-right">
+          <div className="home__hero-right-image">
+
+          </div>
+        </div>
       </div>
       <div className="testnet__header-container">
-          <div className="testnet__line"></div>
+        <div className="testnet__line"></div>
         <div className="home__testnet-header">
           <h3 className="home__testnet-headerText">Testnet Statistics</h3>
         </div>
         <div className="testnet__line"></div>
-        </div>
+      </div>
       <div className="home__testnet">
-       <div className="home__testnet-statistics">
-        {/* <div className="home__testnet-statistics-image">
+        <div className="home__testnet-statistics">
+          {/* <div className="home__testnet-statistics-image">
           <img src={testnet} alt="" />
         </div> */}
-        
-    
-  
-  <div className="home__testnet-statistic">
-    <span className="testnet-total">$7,486,600</span>
-    <span className="testnet-category">Total Value <br />
-Locked</span> 
-</div>
-  <div className="home__testnet-statistic">
-    <span className="testnet-total">3,257</span>
-    <span className="testnet-category">rTokens in <br />
-circulation</span>
-  </div>
-  <div className="home__testnet-statistic">
-    <span className="testnet-total">2500+</span>
-    <span className="testnet-category"> Unique contract
-interactions</span>
-  </div>
-  <div className="home__testnet-statistic">
-    <span className="testnet-total">750+</span>
-    <span className="testnet-category"> Users</span>
-  </div>
-       </div>
-      
+
+
+
+          <div className="home__testnet-statistic">
+            <span className="testnet-total">$7,486,600</span>
+            <span className="testnet-category">Total Value <br />
+              Locked</span>
+          </div>
+          <div className="home__testnet-statistic">
+            <span className="testnet-total">3,257</span>
+            <span className="testnet-category">rTokens in <br />
+              circulation</span>
+          </div>
+          <div className="home__testnet-statistic">
+            <span className="testnet-total">2500+</span>
+            <span className="testnet-category"> Unique contract
+              interactions</span>
+          </div>
+          <div className="home__testnet-statistic">
+            <span className="testnet-total">750+</span>
+            <span className="testnet-category"> Users</span>
+          </div>
+        </div>
+
       </div>
       <div className="testnet__bottom-line"></div>
       <div className="home__about">
@@ -99,73 +99,81 @@ interactions</span>
         <div className="home__empowering-diagram">
           <div className="home__empowering-diagrams-image">
             <div className="home__diagrams-line"></div>
-          <img src={diagram} alt="" />
+            <img src={diagram} alt="" />
           </div>
-        <div className="home__empowering-diagrams">
-        <div className="home__empowering-diagram-section">
-          <h3>Ensures the project's commitment to decentralization</h3>
-         </div>
-         <div className="home__empowering-diagram-section">
-          <h3>Superior decision-making compared to a single entity</h3>
-         </div>
-         <div className="home__empowering-diagram-section">
-          <h3>Upgradability to adapt to changes in EigenLayer protocol</h3>
-         </div>
-         <div className="home__empowering-diagram-section">
-          <h3>Promotes long-term efficiency and stability</h3>
-         </div>
-         
-        </div>
+          <div className="home__empowering-diagrams">
+            <div className="home__empowering-diagram-section">
+              <h3>Ensures the project's commitment to decentralization</h3>
+            </div>
+            <div className="home__empowering-diagram-section">
+              <h3>Superior decision-making compared to a single entity</h3>
+            </div>
+            <div className="home__empowering-diagram-section">
+              <h3>Upgradability to adapt to changes in EigenLayer protocol</h3>
+            </div>
+            <div className="home__empowering-diagram-section">
+              <h3>Promotes long-term efficiency and stability</h3>
+            </div>
+
+          </div>
         </div>
         <div className="about-vectors">
-         <div className="empowering__small-ball"></div>
-         <div className="empowering__full-ball"></div>
+          <div className="empowering__small-ball"></div>
+          <div className="empowering__full-ball"></div>
         </div>
       </div>
       <div className="home__costs">
-      <h2>Zero Costs, Maximum Gains</h2>
-      <p>Experience the freedom of capital re-utilization with zero minting and redeeming fees. Nova Finance is committed to providing a seamless and cost-effective staking experience for our users.</p>
+        <h2>Zero Costs, Maximum Gains</h2>
+        <p>Experience the freedom of capital re-utilization with zero minting and redeeming fees. Nova Finance is committed to providing a seamless and cost-effective staking experience for our users.</p>
       </div>
       <div className="home__roadmap">
         <div className="home__roadmap-header">
           <h3>Our Roadmap:</h3>
         </div>
-        <div styleName='home__roadmap-container'>
+        <div className='home__roadmap-container'>
           <div className="home__roadmap-container-top">
-            
+          <h5 className="home__roadmap-container-bottom-title">R&D nLRP <br />
+              Grant</h5>
+            <h5 className="home__roadmap-container-bottom-title-two" style={{display:"none"}}>Public <br />
+Testnet</h5>
+            <h5 className="home__roadmap-container-bottom-title">Mainnet</h5>
           </div>
           <div className="home__roadmap-container-image">
             <img src={roadmap} alt="" />
           </div>
           <div className="home__roadmap-container-bottom">
-
+            <h5 className="home__roadmap-container-bottom-title">Eth Foundation <br />
+              Grant</h5>
+            <h5 className="home__roadmap-container-bottom-title-two">Public <br />
+Testnet</h5>
+            <h5 className="home__roadmap-container-bottom-title">Puffer AVSs</h5>
           </div>
-        <div className="home__roadmap-vectors">
-        <div className="home__roadmap-flash-one"></div>
-        <div className="home__roadmap-flash-four"></div>
-        <div className="home__roadmap-flash-three"></div>
-        <div className="home__roadmap-flash-two"></div>
-          <div className="home__roadmap-ball-one"></div>
-          <div className="home__roadmap-ball-two"></div>
-          <div className="home__roadmap-ball-three"></div>
-          <div className="home__roadmap-planet"></div>
-          <div className="home__roadmap-star"></div>
-        </div>
+          <div className="home__roadmap-vectors">
+            <div className="home__roadmap-flash-one"></div>
+            <div className="home__roadmap-flash-four"></div>
+            <div className="home__roadmap-flash-three"></div>
+            <div className="home__roadmap-flash-two"></div>
+            <div className="home__roadmap-ball-one"></div>
+            <div className="home__roadmap-ball-two"></div>
+            <div className="home__roadmap-ball-three"></div>
+            <div className="home__roadmap-planet"></div>
+            <div className="home__roadmap-star"></div>
+          </div>
           <div className="testnet__bottom-line"></div>
         </div>
       </div>
-    
-       <div className="home__advantages-container">
-       <div className="home__advantages">
-        <img src={token} alt="" />
+
+      <div className="home__advantages-container">
+        <div className="home__advantages">
+          <img src={token} alt="" />
+        </div>
       </div>
-       </div>
       <div className="home__unravel">
-  <div className="home__unravel-header">
-    <h3>Unraveling RSTK</h3>
-    <p>Discover the power of RSTK, the backbone of Nova Finance's staking platform. Be a pioneer by participating in the first modular liquid restaking protocol powered by EigenLayer.</p>
-    <Link to="/explore" className='home__unravel-link'>Explore More</Link>
-</div>
+        <div className="home__unravel-header">
+          <h3>Unraveling RSTK</h3>
+          <p>Discover the power of RSTK, the backbone of Nova Finance's staking platform. Be a pioneer by participating in the first modular liquid restaking protocol powered by EigenLayer.</p>
+          <Link to="/explore" className='home__unravel-link'>Explore More</Link>
+        </div>
       </div>
     </div>
   )
